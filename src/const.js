52ccf30399f21411105d39418c69387064bcb4bb
Yan Shatskiy <60981097+jan-web@users.noreply.github.com>
2024-07-08 09:21:27 +0200

    I cant get data from server

--- src/const.js
+++ src/const.js
@@ -1,1 +1,2 @@
+// export const API_URL = 'https://milano-api.onrender.com';
 export const API_URL = 'https://milano-api.onrender.com';
--- src/modules/Goods/Goods.jsx
+++ src/modules/Goods/Goods.jsx
@@ -8,27 +8,51 @@ import { useEffect } from 'react';
 
 export const Goods = () => {
 	const dispatch = useDispatch();
-	const { items: goods, status:  goodsStatus, error} = useSelector(state => state.goods);
+	const {
+		items: goods,
+		status: goodsStatus,
+		error,
+	} = useSelector(state => state.goods);
 
 	useEffect(() => {
-		if(goodsStatus === 'idle') {
+		if (goodsStatus === 'idle') {
 			dispatch(fetchGoods());
 		}
 	}, [dispatch, goodsStatus]);
+
+	let content = null;
+
+	if (goodsStatus === 'loading') {
+		content = <p>Loading...</p>;
+	}
+	if (goodsStatus === 'success') {
+		content = (
+			<ul className='goods__list'>
+				{goods.map(item => (
+					<li className='goods__item' key={item.id}>
+						<Card
+							id={item.id}
+							img={`${API_URL}${item.photoUrl}`}
+							title={item.name}
+							dateDelivery='сегодня в 14:00'
+							price={item.price}
+						/>
+					</li>
+				))}
+			</ul>
+		);
+	}
+
+	if (goodsStatus === 'failed') {
+		content = <p>{error}</p>;
+	}
 
 	return (
 		<section className='goods'>
 			<div className='container goods__container'>
 				<div className='goods__box'>
 					<h2 className='goods__title'>Цветы</h2>
-
-					<ul className='goods__list'>
-						{goods.map(item => (
-							<li className='goods__item' key={item.id}>
-								<Card {...item} />
-							</li>
-						))}
-					</ul>
+					{content}
 				</div>
 
 				<Cart />
